405bd0ff05fe65ab85f93992a5b35a9ffd386ac0
rafaelcloud83 <rafaelcloud83@gmail.com>
2025-11-02 19:04:33 -0300

    criado formulário reativo e validado campos do formulário

--- src/app/categorias/categoria/categoria.component.ts
+++ src/app/categorias/categoria/categoria.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-categoria',
@@ -7,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrl: './categoria.component.scss'
 })
 export class CategoriaComponent {
+  camposForm: FormGroup;
 
+  constructor() {
+    this.camposForm = new FormGroup({
+      nome: new FormControl('', Validators.required),
+      descricao: new FormControl('', Validators.required)
+    });
+  }
+
+  salvar() {
+    this.camposForm.markAllAsTouched();
+    if(this.camposForm.valid) {
+      console.log("Valores do formulário:", this.camposForm.value);
+    }
+  }
+
+  isCampoInvalido(nomeCampo: string): boolean {
+    const campo = this.camposForm.get(nomeCampo);
+    return (campo?.invalid && campo?.touched) && campo?.errors?.['required'];
+  }
 }
