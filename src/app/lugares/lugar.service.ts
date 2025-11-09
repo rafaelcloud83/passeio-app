56fcd7e69d40b7568c784c75ce7ba364e4da57bd
rafaelcloud83 <rafaelcloud83@gmail.com>
2025-11-09 17:38:18 -0300

    implementado modulo de lugares

---
+++ src/app/lugares/lugar.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Lugar } from './lugar';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LugarService {
+
+  constructor(private http: HttpClient) { }
+
+  salvar(lugar: Lugar) : Observable<Lugar> {
+    return this.http.post<Lugar>('http://localhost:3000/lugares', lugar);
+  }
+
+  listarTodos() : Observable<Lugar[]> {
+    return this.http.get<Lugar[]>('http://localhost:3000/lugares');
+  }
+}
